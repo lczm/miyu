8b40a11d115a96c715136a3d9e486eb95a9c8226
lczm <zemingchua@gmail.com>
2021-09-05 22:14:06 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,167 @@
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "render_utils.h"
+
+const int SCREEN_WIDTH  = 1280;
+const int SCREEN_HEIGHT = 800;
+
+// Window that to be rendered to
+SDL_Window*  g_window = nullptr;
+// Surface contained by the window
+SDL_Texture* g_texture = nullptr;
+SDL_Renderer* g_renderer = nullptr;
+
+bool init_window() {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    }
+    // Create window
+    g_window = SDL_CreateWindow("Miyu",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    if (g_window == nullptr) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    } 
+    // Create renderer for window
+    g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+    if (g_renderer == NULL) {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // Initialize PNG loading
+    int img_flags = IMG_INIT_PNG;
+    if (!(IMG_Init(img_flags) & img_flags)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return false;
+    } 
+
+    // Initialize renderer color
+    set_render_color(g_renderer, 255, 255, 255);
+
+    return true;
+}
+
+SDL_Texture* load_texture(std::string path) {
+    // Final texture
+    SDL_Texture* new_texture = nullptr;
+
+    // Load image at specified path
+    SDL_Surface* loaded_surface = IMG_Load(path.c_str());
+
+    if (loaded_surface == nullptr) {
+        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+        return nullptr;
+    }
+
+    // Create texture from surface pixels
+    new_texture = SDL_CreateTextureFromSurface(g_renderer, loaded_surface);
+    if (new_texture == NULL) {
+        printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        return nullptr;
+    }
+
+    // Get rid of old loaded surface
+    SDL_FreeSurface(loaded_surface);
+    return new_texture;
+}
+
+void close_window() {
+    // Free loaded image
+    SDL_DestroyTexture(g_texture);
+    g_texture = nullptr;
+
+    // Destroy window
+    SDL_DestroyRenderer(g_renderer);
+    SDL_DestroyWindow(g_window);
+    g_renderer = nullptr;
+    g_window = nullptr;
+
+    // Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main(int argv, char** args) {
+    bool quit = false;
+    // Event handler
+    SDL_Event e;
+
+    // Start up SDL and create window
+    if (!init_window()) {
+        printf("Failed to initialize\n");
+        return 0;
+    }
+    // Load media
+    // auto test = load_surface("preview.png");
+    g_texture = load_texture("preview.png");
+
+    while (!quit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = true;
+                continue;
+            }
+
+            if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_UP:
+                        printf("Up key\n");
+                        break;
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                }
+            }
+        }
+
+        // Clear screen
+        clear_renderer(g_renderer);
+        // Render texture to screen
+        draw_texture(g_renderer, g_texture);
+
+        // Render red filled quad
+        set_render_color(g_renderer, 255, 0, 0);
+        draw_rect(g_renderer, {SCREEN_WIDTH/4, SCREEN_HEIGHT/4, SCREEN_WIDTH/2, SCREEN_HEIGHT/2});
+
+        // Render green outlined quad
+        set_render_color(g_renderer, 0, 255, 0);
+        draw_rect_outline(g_renderer, {SCREEN_WIDTH/6,
+                                       SCREEN_HEIGHT/6,
+                                       SCREEN_WIDTH*2/3,
+                                       SCREEN_HEIGHT*2/3});
+
+        set_render_color(g_renderer, 255, 255, 0);
+        for (int i = 0; i < SCREEN_HEIGHT; i+=4) {
+            draw_point(g_renderer, SCREEN_WIDTH/2, i);
+        }
+
+        std::vector<SDL_Point> points;
+        for (int i = 0; i < SCREEN_WIDTH; i++) {
+            points.push_back({i, 50});
+        }
+
+        set_render_color(g_renderer, 0, 0, 0);
+        draw_points(g_renderer, &points[0], points.size());
+
+        // Update screen
+        flush_renderer(g_renderer);
+    }
+
+    close_window();
+
+    return 0;
+}
---
+++ render_utils.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+
+inline void clear_renderer(SDL_Renderer* r) {
+    SDL_RenderClear(r);
+}
+
+inline void flush_renderer(SDL_Renderer* r) {
+    SDL_RenderPresent(r);
+}
+
+inline void set_render_color(SDL_Renderer* r, int cr, int cg, int cb, int ca) {
+    SDL_SetRenderDrawColor(r, cr, cg, cb, ca);
+}
+
+inline void set_render_color(SDL_Renderer* r, int cr, int cg, int cb) {
+    SDL_SetRenderDrawColor(r, cr, cg, cb, 255);
+}
+
+inline void set_viewport(SDL_Renderer* r, int x, int y, int w, int h) {
+    SDL_Rect rect = {x, y, w, h};
+    SDL_RenderSetViewport(r, &rect);
+}
+
+inline void set_viewport(SDL_Renderer* r, SDL_Rect rect) {
+    SDL_RenderSetViewPort(r, &rect);
+}
+
+inline void draw_point(SDL_Renderer* r, int x, int y) {
+    SDL_RenderDrawPoint(r, x, y);
+}
+
+inline void draw_points(SDL_Renderer* r, SDL_Point* points, int points_count) {
+    SDL_RenderDrawPoints(r, points, points_count);
+}
+
+inline void draw_line(SDL_Renderer* r, int x1, int y1, int x2, int y2) {
+    SDL_RenderDrawLine(r, x1, y1, x2, y2);
+}
+
+// create a vector/array, pass the first element in as points
+inline void draw_lines(SDL_Renderer* r, SDL_Point* points, int points_count) {
+    SDL_RenderDrawLines(r, points, points_count);
+}
+
+inline void draw_rect(SDL_Renderer* r, SDL_Rect rect) {
+    SDL_RenderFillRect(r, &rect);
+}
+
+inline void draw_rect(SDL_Renderer* r, int x, int y, int w, int h) {
+    SDL_Rect rect = {x, y, w, h};
+    SDL_RenderFillRect(r, &rect);
+}
+
+inline void draw_rect_outline(SDL_Renderer* r, SDL_Rect rect) {
+    SDL_RenderDrawRect(r, &rect);
+}
+
+inline void draw_rect_outline(SDL_Renderer* r, int x, int y, int w, int h) {
+    SDL_Rect rect = {x, y, w, h};
+    SDL_RenderDrawRect(r, &rect);
+}
+
+inline void draw_texture(SDL_Renderer* r, SDL_Texture* t) {
+    SDL_RenderCopy(r, t, nullptr, nullptr);
+}
